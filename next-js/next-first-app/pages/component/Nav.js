cb3bb23fb211754d4ef13c66af58c7ad3ffb1ffb
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-05-04 23:22:51 +0600

    next auth implemented

--- next-js/next-first-app/pages/component/Nav.js
+++ next-js/next-first-app/pages/component/Nav.js
@@ -5,14 +5,32 @@ import Link from 'next/link';
 const Nav = () => {
     return (
         <nav className={navStyles.nav}>
-            <ul>
-                <li>
-                    <Link href='/'>Home</Link>
-                </li>
-                <li>
-                    <Link href='/about'>About</Link>
-                </li>
-            </ul>
+            <div>
+                <ul>
+                    <li style={{ fontWeight: 'bold', fontSize: '22px' }}>
+                        <Link href='/'>WebDev</Link>
+                    </li>
+                    <li>
+                        <Link href='/dashboard'>Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link href='/blog'>Blog</Link>
+                    </li>
+                    <li>
+                        <Link href='/about'>About</Link>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <Link href='#'>Sign In</Link>
+                    </li>
+                    <li>
+                        <Link href='#'>Sign Out</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
